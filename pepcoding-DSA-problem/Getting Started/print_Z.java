390abe525c941f927ce9ba5d12d1a50ef86c23d1
Amitabh Patel <amitabh6991@gmail.com>
2021-01-28 10:47:41 +0530

    print

---
+++ pepcoding-DSA-problem/Getting Started/print_Z.java
@@ -0,0 +1,13 @@
+
+public class print_Z {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("*****");
+        System.out.println("   *");
+        System.out.println("  *");
+        System.out.println(" *");
+        System.out.println("*****");
+	}
+
+}
